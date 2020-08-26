48816eb77beb328bc45bd85e44b4515b5b3c68c8
nillys <DWWM-mathieu@mode83.onmicrosoft.com>
2020-08-26 09:40:35 +0200

    Hot-fix css responsivité start.php

--- Exemple.php
+++ Exemple.php
@@ -23,7 +23,7 @@ $gestionnaire_data = new dataManager($pdo, $_POST);
 
 <header id="header">
   <h1 id="main_title">WILBER</h1>
-  <h6 id="label_version">Version Alpha(0.05)</h6>
+  <h6 id="label_version">Version Alpha(0.0.06)</h6>
   <p>Content managing made : <em>easy fresh and simple !</em> (for developper)</p>
 
 </header>
